0ddaea4e3b32842c6fb09f6357e11d21a6f45432
Eric Katz <ericpkatz@gmail.com>
2015-04-15 23:49:23 +0000

    set up controllers and nav

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+
+var app = express();
+
+app.set("view engine", "jade");
+
+app.use(express.static(__dirname + "/client"));
+
+app.use(function(req, res, next){
+  req.foo = Math.random().toString();
+  next();
+});
+
+var paths = ["/", "/people", "/things"];
+
+paths.forEach(function(path){
+  app.get(path, function(req, res, next){
+    res.render("index");
+  });
+});
+
+
+app.listen(process.env.PORT);
