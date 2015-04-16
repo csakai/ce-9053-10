2a2390d681a2b3a3e97d3dafaaa207adb9d986cd
Eric Katz <ericpkatz@gmail.com>
2015-04-16 00:37:11 +0000

    added get endpoint

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var express = require("express");
+var mongoose = require("mongoose");
+
+var PersonSchema = new mongoose.Schema({
+  name: String  
+});
+
+var Person = mongoose.model("Person", PersonSchema);
+
+mongoose.connect("mongodb://localhost/my_world");
+mongoose.connection.once("open", function(){
+  console.log("i am connected");
+});
+
 
 var app = express();
 
-app.locals.pretty = true
+app.locals.pretty = true;
 
 app.set("view engine", "jade");
 
@@ -19,6 +32,13 @@ paths.forEach(function(path){
   app.get(path, function(req, res, next){
     res.render("index");
   });
+});
+
+//api
+app.get("/api/people", function(req, res){
+  Person.find({}).sort("name").exec(function(err, people){
+    res.send(people);
+  }); 
 });
 
 
